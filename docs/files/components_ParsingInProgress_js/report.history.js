b04323a5f602e6eb2db0e7e987acf634946c5b9a
flubox <francois.lunette@photobox.com>
2017-07-11 15:35:29 +0200

    update

--- docs/files/components_ParsingInProgress_js/report.history.js
+++ docs/files/components_ParsingInProgress_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 11 Jul 2017 13:34:04 GMT","sloc":7,"lloc":1,"functions":1,"deliveredBugs":0.025,"maintainability":100,"lintErrors":[],"difficulty":5.333}]
+__history = [{"date":"Tue, 11 Jul 2017 13:34:04 GMT","sloc":7,"lloc":1,"functions":1,"deliveredBugs":0.025,"maintainability":100,"lintErrors":[],"difficulty":5.333},{"date":"Tue, 11 Jul 2017 13:35:04 GMT","sloc":7,"lloc":1,"functions":1,"deliveredBugs":0.025,"maintainability":100,"lintErrors":[],"difficulty":5.333}]
--- docs/files/services_ErrorService_js/report.history.js
+++ docs/files/services_ErrorService_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 11 Jul 2017 13:34:06 GMT","sloc":17,"lloc":9,"functions":2,"deliveredBugs":0.177,"maintainability":75.479,"lintErrors":1,"difficulty":15.355}]
+__history = [{"date":"Tue, 11 Jul 2017 13:34:06 GMT","sloc":17,"lloc":9,"functions":2,"deliveredBugs":0.177,"maintainability":75.479,"lintErrors":1,"difficulty":15.355},{"date":"Tue, 11 Jul 2017 13:35:06 GMT","sloc":17,"lloc":9,"functions":2,"deliveredBugs":0.177,"maintainability":75.479,"lintErrors":1,"difficulty":15.355}]
